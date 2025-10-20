fb0c0078f2a7fd7fcf39b19a70570cc1d8f181e9
Sagar Devkate <sagardeva1029@gmail.com>
2025-10-20 15:13:12 -0700

    Add madlibs.py game

---
+++ madlibs.py
@@ -0,0 +1,10 @@
+
+verb = input("Enter a verb: ")
+noun1 = input("Enter a noun1: ")
+noun2 = input("Enter a noun2: ")
+
+
+madlib = f"My project was supposed to make a tiny lightbulb glow, but instead, it {verb} something that looked suspiciously like {noun1} in a tutu. \
+The {noun2} didn’t know whether to give me first place or call security."
+
+print(madlib)
