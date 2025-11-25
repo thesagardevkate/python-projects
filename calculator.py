5035baa008aedffd2ef53c29a476c0fc17b8a9c8
Sagar Devkate <sagardeva1029@gmail.com>
2025-11-25 14:23:27 -0800

    add calculator.py

---
+++ calculator.py
@@ -0,0 +1,23 @@
+# Python calculator
+
+operator = input("Enter an operator (+, -, *, /): ")
+num1 = float(input("Enter the 1st numver: "))
+num2 = float(input("Enter the 2nd number: "))
+
+
+if operator == "+":
+    result = num1 + num2
+    print(result)
+elif operator == "-":
+    result = num1 - num2
+    print(result)
+elif operator == "*":
+    result = num1 * num2
+    print(result)
+elif operator == "/":
+    result = num1/num2
+    print(result)
+else: 
+    print(f"{operator} is not a valid operator")
+    
+  
