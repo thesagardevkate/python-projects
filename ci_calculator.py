e1367d2511c8b41552d476a239889c6acf8e4baf
Sagar Devkate <sagardeva1029@gmail.com>
2025-11-26 13:26:07 -0800

    add ci_calculator.py

---
+++ ci_calculator.py
@@ -0,0 +1,31 @@
+# Python compound interest calculator
+
+principle = 0
+rate = 0
+time = 0
+
+while principle <= 0:
+    principle = float(input("Enter the principle amount: "))
+    if principle <= 0:
+        print("principle can't be less than or equal to 0")
+  
+
+while rate <= 0:
+    rate = float(input("Enter the interest rate amount: "))
+    if rate <= 0:
+        print("Interest rate can't be less than or equal to 0")
+
+while time <= 0:
+    time = int(input("Enter the time in years: "))
+    if time <= 0:
+        print("Time can't be less than or equal to 0")
+
+
+total = principle * pow((1 + rate / 100), time)
+
+print(f"Balance after {time} year/s: ${total:.2f}")
+
+
+  
+
+
