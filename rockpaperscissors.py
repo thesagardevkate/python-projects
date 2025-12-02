84d1abb8c808988b399de55fbd1c560615c79024
Sagar Devkate <sagardeva1029@gmail.com>
2025-12-01 22:59:31 -0800

    add rockpaperscissors.py

---
+++ rockpaperscissors.py
@@ -0,0 +1,40 @@
+import random
+
+options = ("rock", "paper", "scissors")
+
+running = True
+
+while running:
+
+  player = None 
+  computer = random.choice(options)
+
+
+  while player not in options:
+      player = input("Enter a choice (rock, paper scissors): ")
+
+  print(f"Player: {player}")
+
+  print(f"computer: {computer}")
+
+  if player == computer:
+      print("It's a tie!")
+  elif player == "rock" and computer == "scissors":
+      print("You win!")
+  elif player == "paper" and computer == "rock":
+      print( "You win!")
+  elif player == "scissors" and computer == "paper":
+      print("You win!")
+  else:
+      print("You lose!")
+
+
+
+      play_again = input("Play again? (y/n): ").lower()
+      if not play_again == "y":
+          running = False
+
+print("Thanks for playing")
+    
+    
+
